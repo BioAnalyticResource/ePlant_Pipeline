ad75ddb48d1781603a16ad0799f93a51911a1cad
Asher <asher.pasha@utoronto.ca>
2017-05-19 17:10:47 -0400

    Added XML processing script.

---
+++ efp_views/updateXML.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This program updates the XML file and add IDs
+Author: Asher
+Date: May, 2017
+Usage: python3 updateXML.py
+"""
+import sys
+import xml.etree.ElementTree as ET
+
+def main():
+	"""
+	The main program for rewriting XML file
+
+	:return: 0 or 1, 0 for success
+	"""
+
+	# Using Poplar data as a example
+	oldXML = "Populus_trichocarpa.xml"
+	newXML = "Populus_trichocarpa_new.xml"	# Rename this when copying to ePlant
+
+	try:
+		tree = ET.parse(oldXML)
+		root = tree.getroot()
+	except:
+		print("Failed to parse the XML file.")
+		return 1
+
+	# Add and id to tissue key, which will be used by ePlant to color SVG file.
+	# The ID here should match ID in SVG file
+	for tissue in root.iter('tissue'):
+		name = tissue.get('name')
+		name = name.replace(' ', '_')
+		tissue.set('id', name)
+		
+	tree.write(newXML)
+
+	return 0
+
+if __name__ == '__main__':
+	main()
