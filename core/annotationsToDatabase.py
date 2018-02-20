6f6f6cb84fd3eb6e84acd295cc61774e784c7ad0
Asher <asher.pasha@utoronto.ca>
2018-02-20 17:19:33 -0500

    Updated some files.

--- core/annotationsToDatabase.py
+++ core/annotationsToDatabase.py
@@ -8,92 +8,84 @@ Usage: Add passwords to dbConfig.py file and run: Python3 annotationsToDatabase.
 import sys
 import re
 import MySQLdb
-import warnings
+
 
 def connect():
-	"""
-	Connect to the database
+    """
+    Connect to the database
 
-	:returns conn: A connections
-	"""
+    :returns conn: A connections
+    """
 
-	# Supress MySQL Warnings
-	warnings.filterwarnings("ignore", category = MySQLdb.Warning)
+    try:
+        # Connect to database and get a cursor
+        conn = MySQLdb.connect(host='', user='', passwd='', db='')
+    except MySQLdb.Error:
+        print('Can not connect to database!')
+        sys.exit(-1)
 
-	try:
-		# Connect to database and get a cursor
-		conn = MySQLdb.connect(host='', user='', passwd='', db='')
-		cur = conn.cursor()
-	except MySQLdb.Error:
-		print('Can not connect to database!')
-		sys.exit(1)
-	
-	return conn
+    return conn
+
 
 def main():
-	"""
-	The main program for annotations to database program
-	
-	:returns: 0 or 1, 0 for success
-	"""
+    """
+    The main program for annotations to database program
 
-	# Suppress MySQL Warnings
-	warnings.filterwarnings("ignore", category = MySQLdb.Warning)
+    :returns: 0 or 1, 0 for success
+    """
 
-	# Variables
-	annotationsFile = 'Ptrichocarpa_210_v3.0.defline.txt'	# Annotations File
-	commentLineRegEx = re.compile('^#')	# Comments
-	
-	# Read Annotations Data
-	try:
-		annotationsfh = open(annotationsFile, 'r')
-	except:
-		print('An error has occured: ', sys.exc_info()[0])
-		raise
-		return 1
+    # Variables
+    annotationsFile = 'Spurpurea_289_v1.0.defline.txt'  # Annotations File
+    commentLineRegEx = re.compile('^#')  # Comments
 
-	# Get the connection
-	conn = connect()
-	cursor = conn.cursor();
+    # Read Annotations Data
+    try:
+        annotationsfh = open(annotationsFile, 'r')
+    except FileNotFoundError:
+        print('An error has occurred: ', sys.exc_info()[0])
+        sys.exit(-1)
 
-	for line in annotationsfh:
-		line = line.rstrip()
-		line = line.rstrip("\n\r")
+    # Get the connection
+    conn = connect()
+    cursor = conn.cursor()
 
-		# GFF3 has comment starting with '#"
-		matchComment = commentLineRegEx.match(line)
-		if matchComment:
-			# Comment is found. move the next line
-			continue	
-		
-		# Split the remaining lines for database table
-		columns = line.split("\t")
-		
-		# Now insert into database
-		try:
-			sql = "INSERT INTO gene_annotation (gene, annotation) VALUES (%s, %s)"
-			cursor.execute(sql, (columns[0], columns[1]))
-			cursor.execute("SHOW WARNINGS")
-			sqlWarnings = cursor.fetchall()
-			for i in range(len(sqlWarnings)):
-				print("Warning - " +sqlWarnings[i][2])
-		except MySQLdb.Error as e:
-			print("Error inserting data into database:" + e.args[0] + " + " + e.args[1])
-			conn.rollback()
-			return 1
+    for line in annotationsfh:
+        line = line.rstrip()
+        line = line.rstrip("\n\r")
 
-	try:
-		conn.commit()
-	except:
-		print("Commit to database failed!")
-		conn.rollback()
-		return 1
+        # GFF3 has comment starting with '#"
+        matchComment = commentLineRegEx.match(line)
+        if matchComment:
+            # Comment is found. move the next line
+            continue
 
-	conn.close()
-	annotationsfh.close()
-	return 0
+        # Split the remaining lines for database table
+        columns = line.split("\t")
+
+        # Now insert into database
+        sql = "INSERT INTO gene_annotation (gene, annotation) VALUES (%s, %s)"
+        try:
+            cursor.execute(sql, (columns[0], columns[2]))
+        except MySQLdb.Error as e:
+            print("Error inserting data into database: Error: " + str(e.args[0]) + ": " + e.args[1])
+            conn.rollback()
+            conn.close()
+            annotationsfh.close()
+            sys.exit(-1)
+
+    try:
+        conn.commit()
+    except MySQLdb.Error:
+        print("Commit to database failed!")
+        annotationsfh.close()
+        conn.rollback()
+        conn.close()
+        sys.exit(-1)
+
+    conn.close()
+    annotationsfh.close()
+    return 0
+
 
 if __name__ == '__main__':
-	main()
-	
-
+    main()
--- core/gff3ToDatabase.py
+++ core/gff3ToDatabase.py
@@ -8,132 +8,133 @@ Usage: Add passwords to dbConfig.py file and run: Python3 gff3ToDatabase.py
 import sys
 import re
 import MySQLdb
-import warnings
+
 
 def connect():
-	"""
-	Connect to the database
+    """
+    Connect to the database
 
-	:returns conn: A connections
-	"""
+    :returns conn: A connections
+    """
 
-	# Supress MySQL Warnings
-	warnings.filterwarnings("ignore", category = MySQLdb.Warning)
+    try:
+        # Connect to database and get a cursor
+        conn = MySQLdb.connect(host='', user='', passwd='', db='')
+    except MySQLdb.Error:
+        print('Can not connect to database!')
+        sys.exit(-11)
 
-	try:
-		# Connect to database and get a cursor
-		conn = MySQLdb.connect(host='', user='', passwd='', db='')
-		cur = conn.cursor()
-	except MySQLdb.Error:
-		print('Can not connect to database!')
-		sys.exit(1)
-	
-	return conn
+    return conn
+
 
 def main():
-	"""
-	The main program for gff3 to database program
-	
-	:returns: 0 or 1, 0 for success
-	"""
+    """
+    The main program for gff3 to database program
 
-	# Variables
-	gff3File = 'Ptrichocarpa_210_v3.0.gene_exons.gff3'	# GFF3 file
-	commentLineRegEx = re.compile('^#')	# Comments
-	parentRegEx = re.compile('Parent=(.*?);')	# Parent column
-	idRegEx = re.compile('ID=(.*?);')	# ID column
-	nameRegEx = re.compile('Name=(.*?);')	# Name
+    :returns: 0 or 1, 0 for success
+    """
 
-	# Read GFF3 Data
-	try:
-		gff3fh = open(gff3File, 'r')
-	except:
-		print('An error has occured: ', sys.exc_info()[0])
-		raise
-		return 1
+    # Variables
+    gff3File = 'Spurpurea_289_v1.0.gene_exons.gff3'  # GFF3 file
+    commentLineRegEx = re.compile('^#')  # Comments
+    parentRegEx = re.compile('Parent=(.*?);')  # Parent column
+    idRegEx = re.compile('ID=(.*?);')  # ID column
+    nameRegEx = re.compile('Name=(.*?);')  # Name
 
-	# Get the connection
-	conn = connect()
-	cursor = conn.cursor();
+    # Read GFF3 Data
+    try:
+        gff3fh = open(gff3File, 'r')
+    except FileNotFoundError:
+        print('An error has occurred: ', sys.exc_info()[0])
+        sys.exit(-1)
 
-	for line in gff3fh:
-		line = line.rstrip()
-		line = line.rstrip("\n\r")
+    # Get the connection
+    conn = connect()
+    cursor = conn.cursor()
 
-		# GFF3 has comment starting with '#"
-		matchComment = commentLineRegEx.match(line)
-		if matchComment:
-			# Comment is found. move the next line
-			continue	
-		
-		# Split the remaining lines for database table
-		columns = line.split("\t")
-		
-		# Get ID
-		matchId = idRegEx.search(columns[8])
-		if matchId:
-			geneId = matchId.group(1)
-		else:
-			geneId = ""
-			print("Warning: No ID!")
-		
-		# Get Parent
-		matchParent = parentRegEx.search(columns[8])
-		if matchParent:
-			parent = matchParent.group(1)
-		else:
-			if geneId != "":
-				parent = geneId
-			else:
-				print("Warning: No parrent!")
-		
-		# Get name
-		geneName = None
-		matchName = nameRegEx.search(columns[8])
-		if matchName:
-			geneName = matchName.group(1)
+    for line in gff3fh:
+        line = line.rstrip()
+        line = line.rstrip("\n\r")
 
-		# Replace '.' by NULL
-		if columns[5] == ".":
-			columns[5] = None
+        # GFF3 has comment starting with '#"
+        matchComment = commentLineRegEx.match(line)
+        if matchComment:
+            # Comment is found. move the next line
+            continue
 
-		if columns[7] == ".":
-			columns[7] = None
-		
-		if (geneName != None):	
-			geneName = re.sub(r'.v3.0', '', geneName)
-	
-		if (geneId != None):
-			geneId = re.sub(r'.v3.0', '', geneId)
+        # Split the remaining lines for database table
+        columns = line.split("\t")
 
-		if (parent != None):
-			parent  = re.sub(r'.v3.0', '', parent)
+        # Skip the scaffolds
+        if 'Scaffold' in columns[0]:
+            continue
 
-		# Now insert into database
-		try:
-			sql = "INSERT INTO gff3_v3 (SeqID, Source, Type, Start, End, Score, Strand, Phase, Id, geneId, Parent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-			cursor.execute(sql, (columns[0], columns[1], columns[2], columns[3], columns[4], columns[5], columns[6], columns[7], geneName, geneId, parent))
-			cursor.execute("SHOW WARNINGS")
-			sqlWarnings = cursor.fetchall()
-			for i in range(len(sqlWarnings)):
-				print("Warning - " +sqlWarnings[i][2])
-		except MySQLdb.Error as e:
-			print("Error inserting data into database: :" + e.args[0] + " + " + e.args[1])
-			conn.rollback()
-			return 1
+        # Get ID
+        matchId = idRegEx.search(columns[8])
+        if matchId:
+            geneId = matchId.group(1)
+        else:
+            geneId = ""
+            print("Warning: No ID!")
 
-	try:
-		conn.commit()
-	except:
-		print("Commit to database failed!")
-		conn.rollback()
-		return 1
+        # Get Parent
+        matchParent = parentRegEx.search(columns[8])
+        if matchParent:
+            parent = matchParent.group(1)
+        else:
+            if geneId != "":
+                parent = geneId
+            else:
+                print("Warning: No parrent!")
 
-	conn.close()
-	gff3fh.close()
-	return 0
+        # Get name
+        geneName = None
+        matchName = nameRegEx.search(columns[8])
+        if matchName:
+            geneName = matchName.group(1)
+
+        # Replace '.' by NULL
+        if columns[5] == ".":
+            columns[5] = None
+
+        if columns[7] == ".":
+            columns[7] = None
+
+        if geneName is not None:
+            geneName = re.sub(r'.v1.0', '', geneName)
+
+        if geneId is not None:
+            geneId = re.sub(r'.v1.0', '', geneId)
+
+        if parent is not None:
+            parent = re.sub(r'.v1.0', '', parent)
+
+        # Now insert into database
+        sql = "INSERT INTO gff3 (SeqID, Source, Type, Start, End, Score, Strand, Phase, Id, geneId, Parent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        try:
+            cursor.execute(sql, (
+                columns[0], columns[1], columns[2], columns[3], columns[4], columns[5], columns[6], columns[7], geneName, geneId, parent)
+            )
+        except MySQLdb.Error as e:
+            print("Error inserting data into database: Error: " + str(e.args[0]) + ": " + e.args[1])
+            conn.rollback()
+            conn.close()
+            gff3fh.close()
+            sys.exit(-1)
+
+    try:
+        conn.commit()
+    except MySQLdb.Error:
+        print("Commit to database failed!")
+        conn.rollback()
+        conn.close()
+        gff3fh.close()
+        sys.exit(-1)
+
+    conn.close()
+    gff3fh.close()
+    sys.exit(-1)
+
 
 if __name__ == '__main__':
-	main()
-	
-
+    main()
